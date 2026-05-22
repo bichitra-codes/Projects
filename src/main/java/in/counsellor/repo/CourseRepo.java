60c3ca0f398752e8f7fd464a5cd16fdad1b57490
Bichitra <goudabichitra68@gmail.com>
2026-05-22 22:47:40 +0530

    Removed old file

--- src/main/java/in/counsellor/repo/CourseRepo.java
+++
@@ -1,9 +0,0 @@
-package in.counsellor.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import in.counsellor.entity.Course;
-
-public interface CourseRepo extends JpaRepository<Course, Integer> {
-
-}
